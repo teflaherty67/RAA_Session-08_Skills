5b7278a74926c2ddb922d6c280fadc73c64d6845
Tom E Flaherty <teflaherty@utexas.edu>
2023-02-03 18:28:31 -0600

    Add project files.

---
+++ RAA_Session 08_Skills/Command.cs
@@ -0,0 +1,38 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+#endregion
+
+namespace RAA_Session_08_Skills
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            string userName = app.Username;
+            string filePath = doc.PathName;
+            string date = DateTime.Now.ToString();
+
+            string curAssembly = System.Reflection.Assembly.GetExecutingAssembly().FullName;
+            string curMethod = this.GetType().Name;
+
+            return Result.Succeeded;
+        }
+    }
+}
